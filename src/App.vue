c45c12dac575864bb4895944d29a196310766c34
yssjtu <635664803@qq.com>
2017-11-18 22:06:53 +0800

    prototype v1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>{{title}}</h1>
+    <input v-model="newitem" v-on:keyup.enter="addnew">
+    <ul>
+      <li v-bind:class="{finish: item.isFinished}" v-for="item in items" v-on:click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      title: 'this is a todo list',
+      items: [
+        {
+            label: 'coding',
+            isFinished: false
+        },
+        {
+            label: 'wal',
+            isFinished: true
+        }
+      ],
+      newitem: ''
+    }
+  },
+  methods: {
+      toggleFinish: function (item) {
+        item.isFinished=!item.isFinished;
+      },
+      addnew: function () {
+        console.log(this.newitem)
+        this.items.push({
+          label: this.newitem,
+          isFinished: false
+        })
+        this.newitem= ''
+      }
   }
 }
 </script>
 
 <style>
-#app {
+  .finish{
+    text-decoration: underline;
+  }
+  html{
+    height: 100%;
+  }
+  body{
+    display: flex;
+    align-items: center;
+  }
+  #app
+  {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
