944d0666d414ff22d1aabe447b48fb5460c3de8b
yssjtu <635664803@qq.com>
2017-11-19 14:00:37 +0800

    a shabby  to-do list

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
-    <input v-model="newitem" v-on:keyup.enter="addnew">
+    <div class="container">
+    <h1 v-text="title"></h1>
+    <input v-model="newitem" v-on:keyup.enter="addnew"><i class="glyphicon glyphicon-plus" @click="addnew"></i>
     <ul>
-      <li v-bind:class="{finish: item.isFinished}" v-for="item in items" v-on:click="toggleFinish(item)">
-        {{item.label}}
+      <li v-bind:class="{finish: item.isFinished}" v-for="(item,index) in items" v-on:click="toggleFinish(item)">
+        <span>{{index+1}}</span><i>{{item.label}}</i>
+        <i class="glyphicon glyphicon-remove" @click="items.splice(index,1)"></i>
       </li>
     </ul>
+    </div>
   </div>
 </template>
 
 <script>
-  import Store from './windowstore'
-  console.log(Store)
+import Store from './windowstore'
 export default {
   data: function () {
     return {
-      title: 'this is a todo list',
+      title: 'ys\'s shabby todo list',
       items: Store.fetch(),
       newitem: ''
     }
@@ -35,26 +37,74 @@ export default {
       },
       addnew: function () {
         console.log(this.newitem)
+        if(this.newitem==='')
+        {
+            return
+        }
         this.items.push({
           label: this.newitem,
           isFinished: false
         })
         this.newitem= ''
-      }
+      },
   }
 }
 </script>
 
 <style>
+  h1{
+    margin-bottom: 8px;
+  }
+  ul {
+    text-align: left;
+    margin-left: 50px;
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    position: relative;
+    font: bold italic 25px/1.5 Helvetica, Verdana, sans-serif;
+    margin-bottom: 20px;
+  }
+
+  li i {
+    font: 20px/1.5 Helvetica, sans-serif;
+    padding-left: 60px;
+    color: #555;
+  }
+  span {
+    color: coral;
+    position: absolute;
+  }
+  .container {
+    text-align: center;
+  }
   .finish{
-    text-decoration: underline;
+    text-decoration: line-through;
+    text-decoration-color: blueviolet;
   }
-  html{
-    height: 100%;
+
+  input {
+    margin-bottom: 40px;
+    box-shadow: 0 0 8px rgb(100, 144, 149);
+    border: none;
+    alignment: center;
   }
-  body{
-    display: flex;
-    align-items: center;
+  button {
+    margin: 3px;
+    border-radius: 4px;
+    color: black;
+    background: none;
+  }
+ i {
+    margin: 5px;
+    font-size: 15px;
+    cursor: pointer;
+    opacity: 0.8;
+    transition: opacity 0.5s ease;
+  }
+ i:hover {
+    opacity: 1;
   }
   #app
   {
@@ -62,7 +112,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #643e50;
   margin-top: 60px;
 }
 </style>
